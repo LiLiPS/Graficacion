53919800dc78c362afbe1d9d66837cbfc9c3f370
LiLiPS <lili98ps@hotmail.com>
2018-10-12 10:13:18 -0500

    Com

--- src/app/gui/Interfaz.java
+++ src/app/gui/Interfaz.java
@@ -10,6 +10,7 @@ import java.awt.Color;
 /**
  *
  * @author lili9
+  Comentario equis.
  */
 public class Interfaz extends javax.swing.JFrame {
     /**
@@ -152,7 +153,7 @@ public class Interfaz extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -204,8 +205,6 @@ public class Interfaz extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
- 
+
 
 }
-
-
